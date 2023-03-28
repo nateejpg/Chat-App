456561abd3995dc3d07ea2678baa3cb08f72756c
NathanAbreu1 <Nathan11.abreu@gmail.com>
2023-03-28 00:43:44 -0300

    Finishing the register page

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Data from "../Data";
+import add from "../images/addavatar.png";
 
-const Register = () => {
+function Register() {
+  const [url, setUrl] = useState("");
+
+  function RandomImage() {
+    useEffect(() => {
+      const randomURL =
+        Data.imgs[Math.floor(Math.random() * Data.imgs.length)].url;
+      setUrl(randomURL);
+    }, []);
+
+    return <img src={url} alt="Random" />;
+  }
+
   return (
-    <div className="formContainer">
-      <div className="formWrapper">
-        <span className="logo">TalkNow</span>
+    <div className="RegisterContainer">
+      <div className="imgContainer">
+        <RandomImage />
+      </div>
+      <div className="formContainer">
+        <h1 className="logo">TalkNow</h1>
         <span className="title">Register</span>
         <form>
           <input placeholder="Display Name" type="name"></input>
           <input placeholder="Email" type="email"></input>
           <input placeholder="Password" type="password"></input>
-          <input type="File"></input>
+          <input type="file" style={{ display: "none" }} id="file"></input>
+          <label htmlFor="file" className="adding">
+            <img src={add} alt=""></img>
+            <p>Add an avatar</p>
+          </label>
           <button>Sign Up</button>
         </form>
-        <p>You do have an accout? Login</p>
+        <p>You do not have an account? Login</p>
       </div>
     </div>
   );
-};
+}
 
 export default Register;
---
+++ src/Data.js
@@ -0,0 +1,30 @@
+const Data = {
+    imgs: [
+      {
+        id: 1,
+        url: "https://i.etsystatic.com/41354179/r/il/d809d8/4695775000/il_fullxfull.4695775000_qd6l.jpg",
+      },
+      {
+        id: 2,
+        url: "https://images.nightcafe.studio/jobs/aT9pR5l3WQrRPR5iyDVl/aT9pR5l3WQrRPR5iyDVl--1--E0VGH_11.3636x.jpg?tr=w-1600,c-at_max",
+      },
+      {
+        id: 3,
+        url: "https://images.nightcafe.studio/jobs/AyZ37blHIZ0JuOGC0rsU/AyZ37blHIZ0JuOGC0rsU--3--dr6yg_4x.jpg?tr=w-1600,c-at_max",
+      },
+      {
+        id: 4,
+        url: "https://kawaiiai.com/wp-content/uploads/2022/09/surreal-portrait-ai-art-3.jpg",
+      },
+      {
+        id: 5,
+        url: "https://media-cdn.deca.art/tezos/KT1LjmAdYQCLBjwv4S2oFkEzyHVkomAf5MrW/26907/original.png?tx=w_1200,c_limit",
+      },
+      {
+        id: 6,
+        url: "https://kawaiiai.com/wp-content/uploads/2022/09/surreal-portrait-ai-art-4.jpg",
+      },
+    ],
+  }
+
+export default Data;
