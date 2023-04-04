071670a4e1d0e925912ed2a0bd31522a6ca31f1b
NathanAbreu1 <Nathan11.abreu@gmail.com>
2023-04-04 20:07:41 -0300

    adding the input bar

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Attach from "../images/attach.png";
+import Img from "../images/imgback.png";
 
-const Input = () => {
-  return <div className="input">Input</div>;
-};
+function Input() {
+  return (
+    <div className="input">
+      <input type="text" placeholder="Type something..." />
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input type="file" style={{ display: "none" }} id="file" />
+        <label htmlFor="file">
+          <img src={Img} alt=""></img>
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
+  );
+}
 
 export default Input;
