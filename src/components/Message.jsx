833105283ea8994e6bfbbec99b4b12695ba55dd6
NathanAbreu1 <Nathan11.abreu@gmail.com>
2023-04-04 20:47:24 -0300

    updating the chat

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Message = () => {
-  return <div className="message">Message</div>;
+  return (
+    <div className="message">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/262391/pexels-photo-262391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        ></img>
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img
+          src="https://images.pexels.com/photos/262391/pexels-photo-262391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        ></img>
+      </div>
+    </div>
+  );
 };
 
 export default Message;
